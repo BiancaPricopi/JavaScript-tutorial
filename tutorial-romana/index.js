52c9ed64e153bdc745bf0b40be7b770ed89e0d9f
Pricopi Bianca <pb31627@gmail.com>
2022-03-01 19:39:41 +0200

    [index.js] operators

--- tutorial-romana/index.js
+++ tutorial-romana/index.js
@@ -2,42 +2,18 @@
 //let locale, var globale
 //good practice: let + const
 
-let isMinor = true; //boolean
-console.log(typeof(isMinor));
+const day1 = "Monday";
+const diff = 2 - 1; // /, +, *
+let x = 0;
+x++;
+console.log("x = " + x);
+x--;
 
-let n = null;
-console.log(typeof(n));
+// logici: &&, ||, !
 
-let undf = undefined;
-console.log(typeof(undf)); //variabile nedefinite
-
-let num = 120;
-console.log(typeof(num));
-
-let str = "hello";
-console.log(typeof(str));
-
-//obiect 
-let person = {
-    age : 20,
-    name : "Bogdan"
-}
-console.log(person);
-
-//array
-const hobbies = ["sport", "reading", "cooking", "coding maybe"];
-const prices = [22.99, 1.8, 10.99];
-console.log(hobbies);
-console.log(prices);
-
-const biking = hobbies[1];
-
-let person2 = {
-    age : 10,
-    name : "Stef"
-}
-
-const persons = [person, person2];
-console.log(persons);
-const student = [{id : "RSL23", grade : 10}, {id : "RSL24", grade : 9}]; //inline
-console.log(student);
+// ==, !=, >, >=, <, <=, ===, !==
+const num1 = 9;
+const num2 = 10;
+const num3 = "9";
+console.log(num1 == num3);
+console.log(num1 === num3);
