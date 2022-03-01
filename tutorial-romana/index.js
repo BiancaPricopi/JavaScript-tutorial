573dcb5af78547140b15d6a2c31b8a3ad408eeda
Pricopi Bianca <pb31627@gmail.com>
2022-03-01 20:07:01 +0200

    [index.js] if/else debug

--- tutorial-romana/index.js
+++ tutorial-romana/index.js
@@ -2,18 +2,14 @@
 //let locale, var globale
 //good practice: let + const
 
-const day1 = "Monday";
-const diff = 2 - 1; // /, +, *
-let x = 0;
-x++;
-console.log("x = " + x);
-x--;
+let age = 10;
 
-// logici: &&, ||, !
-
-// ==, !=, >, >=, <, <=, ===, !==
-const num1 = 9;
-const num2 = 10;
-const num3 = "9";
-console.log(num1 == num3);
-console.log(num1 === num3);
+debugger;
+if(age > 18)
+{
+    console.log("You are over 18.");
+}
+else
+{
+    console.log("You are under 18.");
+}
