4347ece39f0b738ad36f7913a3ec7d2fbcbc2878
Pricopi Bianca <pb31627@gmail.com>
2022-03-01 19:29:09 +0200

    [index.js] arrays

--- tutorial-romana/index.js
+++ tutorial-romana/index.js
@@ -23,3 +23,21 @@ let person = {
     name : "Bogdan"
 }
 console.log(person);
+
+//array
+const hobbies = ["sport", "reading", "cooking", "coding maybe"];
+const prices = [22.99, 1.8, 10.99];
+console.log(hobbies);
+console.log(prices);
+
+const biking = hobbies[1];
+
+let person2 = {
+    age : 10,
+    name : "Stef"
+}
+
+const persons = [person, person2];
+console.log(persons);
+const student = [{id : "RSL23", grade : 10}, {id : "RSL24", grade : 9}]; //inline
+console.log(student);
