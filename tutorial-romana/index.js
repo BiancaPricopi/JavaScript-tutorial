7077f2b6ed2b68c8d2db01229b98e479a82bab99
Pricopi Bianca <pb31627@gmail.com>
2022-03-01 19:10:04 +0200

    [index.js] variabile

--- tutorial-romana/index.js
+++ tutorial-romana/index.js
@@ -1,2 +1,13 @@
-console.log("Codul a fost executat!");
-console.error("Bug");
+//var, let, const
+//let locale, var globale
+//good practice: let + const
+
+console.log(myName);
+const age = 5;
+
+{
+    var myName = "Bianca";
+    let secondName = "Mihai";
+}
+console.log(myName);
+console.log(secondName);
